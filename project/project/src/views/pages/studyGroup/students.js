23506f5ebb2f71f0e52360a00192a6c55ea0d04f
msethi2021 <manmohan.sethi@macrometa.com>
2021-11-13 23:07:21 +0530

    fixed edit issue

---
+++ project/project/src/views/pages/studyGroup/students.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name:'Study group 1 ',
+        leader:'Narendra Modi',
+        subject:'Science',
+        dateOS:'15/11/2021 10:15AM',
+        sCount:8
+    },
+    {
+        name:'Study group 2 ',
+        leader:'Sonam Kapoor',
+        subject:'Science',
+        dateOS:'15/11/2021 10:15AM',
+        sCount:2
+    },
+    {
+        name:'Study group 3 ',
+        leader:'Narendra Modi',
+        subject:'Science',
+        dateOS:'15/11/2021 10:15AM',
+        sCount:8
+    },
+    {
+        name:'Study group 4 ',
+        leader:'Manmohan Singh',
+        subject:'Science',
+        dateOS:'15/11/2021 10:15AM',
+        sCount:13
+    },
+    {
+        name:'Study group 5 ',
+        leader:'Manmohan Singh',
+        subject:'Science',
+        dateOS:'15/11/2021 10:15AM',
+        sCount:5
+    }
+   
+]
